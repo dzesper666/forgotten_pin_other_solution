ddcbef65880d15c45b7517f61a3673c29fac8ad6
ksarama <ksarama@amctech.pl>
2019-03-19 09:30:36 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PIN_simplest_solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
